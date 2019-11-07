21f31a0a34ced54857c1478354297d42e5fc392c
Otto Cedeno <otto@ottostacos.com>
2019-11-07 21:33:44 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return attendees.collect {|person| "Hello, my name is #{person}."}
+end
+
+def assign_rooms(attendees)
+  return attendees.collect {|person| "Hello, #{person}! You'll be assigned to room #{attendees.index(person) + 1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_rooms(attendees).each {|room| puts room}
+end
+
+# list = ["Otto", "Indy", "Mac", "Joe"]
