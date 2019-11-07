b1735f1296d7b4b591912532882a078a837fbf37
Otto Cedeno <otto@ottostacos.com>
2019-11-07 21:17:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,4 +13,6 @@ def assign_rooms(attendees)
   return attendees.collect {|person| "Hello, #{person}! You'll be assigned to room #{attendees.index(person) + 1}"}
 end
 
+list = ["Otto", "Indy", "Mac", "Joe"]
+
 puts assign_rooms
